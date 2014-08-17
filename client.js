8c34c1900f0558d43bd5ebb6d5ae5e34c8fa0546
johnnyman727 <johnnyman727@gmail.com>
2014-08-16 18:05:41 -0700

    Initial commit

---
+++ client.js
@@ -0,0 +1,37 @@
+var ws = require("nodejs-websocket");
+var port = 8000;
+// You may need to change this in your script
+var numLEDs = 24;
+
+// Set the binary fragmentation to 1 byte so it instantly sends
+// anything we write to it
+ws.setBinaryFragmentation(1);
+
+var numLEDsBuffer = new Buffer(4);
+numLEDsBuffer.writeUInt32BE(numLEDs, 0);
+
+// Make a buffer to store the RGB values for each pixel
+var onAnimation = new Buffer(numLEDs * 3);
+// Make all the pixels white (equal R,G,B)
+onAnimation.fill(0xff);
+
+// Make another buffer to store animation for off buffer
+var offAnimation = new Buffer(numLEDs * 3);
+// Fill it with zero (R,G, and B are all off)
+offAnimation.fill(0x00);
+
+// When we get a connection (Put your Tessel's IP Address here!)
+var connection = ws.connect('ws://192.168.8.110:' + port, function() {
+  // Pipe the data to our server
+  process.stdin.on('data', function(data) {
+    // Check for off command
+    if (data.toString() == 'off\n') {
+      // Send the number of LEDs and the animation data
+      connection.sendBinary(Buffer.concat([numLEDsBuffer, offAnimation]));
+    }
+    else if (data.toString() == 'on\n'){
+      // Send the number of LEDs and the animation data
+      connection.sendBinary(Buffer.concat([numLEDsBuffer, onAnimation]));
+    }
+  })
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+
+var ws = require("nodejs-websocket"),
+    Neopixels = require('neopixels'),
+    neopixels = new Neopixels();
+    tessel = require('tessel');
+    port = 8000,
+
+// Create the websocket server, provide connection callback
+var server = ws.createServer(function (conn) {
+  console.log("Accepted new connection...");
+
+  // If get a binary stream is opened up
+  conn.on("binary", function(stream) {
+    // When we get data
+    stream.on('data', function(data) {
+      // Extract the number of LEDs to animate
+      var numLEDs = data.readInt32BE(0);
+      // Slice off that number
+      var animation = data.slice(4);
+      // Start the animation
+      neopixels.animate(numLEDs, animation);
+    });
+  });
+
+  conn.on("close", function (code, reason) {
+      console.log("Connection closed")
+  });
+}).listen(port);
+
+console.log('listening on port', port);
